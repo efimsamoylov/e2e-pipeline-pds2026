3e07e4dd15375f48660a9de577a858b87d541821
efimsamoylov <efimsamoilov62@gmail.com>
2026-01-31 14:30:54 +0100

    Update config.py

--- Approach 7_Hybrid_Lexicon/config.py
+++ Approach 7_Hybrid_Lexicon/config.py
@@ -5,13 +5,13 @@ BASE_DIR = Path(__file__).resolve().parent
 PROJECT_ROOT = BASE_DIR.parent
 
 DATA_DIR = PROJECT_ROOT / "data"
-# Важно: берем модели, обученные в прошлом подходе
+
 CHECKPOINTS_DIR = PROJECT_ROOT / "Approach 6_updated" / "checkpoints"
 
 # Input Data
 NOT_ANNOTATED_JSON_PATH = DATA_DIR / "linkedin-cvs-not-annotated.json"
 
-# Lexicons (Словари)
+# Lexicons
 DEPT_LEXICON_PATH = DATA_DIR / "department_lexicon.json"
 SEN_LEXICON_PATH = DATA_DIR / "seniority_lexicon.json"
 
@@ -20,5 +20,5 @@ OUTPUT_DIR = BASE_DIR / "Output"
 PREDICTIONS_PATH = OUTPUT_DIR / "predictions.csv"
 
 # Parameters
-DEPT_ML_THRESHOLD = 0.99  # Порог для нейросети
-SEN_ML_THRESHOLD = 0.95   # Порог для нейросети
+DEPT_ML_THRESHOLD = 0.99
+SEN_ML_THRESHOLD = 0.95
